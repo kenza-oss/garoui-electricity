c3953d698361bec9220036a58532d274d2108406
kenza-oss <mahlebkenza1@gmail.com>
2026-05-23 16:33:59 +0200

    fix: remove compile_messages from build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Compile translation files (.po -> .mo)
-python compile_messages.py
-
 python manage.py collectstatic --no-input
 python manage.py migrate
